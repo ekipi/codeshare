177ed398941e7c3ea818795f46233c8f54c1a322
chiranjeevi tapal <chiranjeevi.tapal@outlook.com>
2018-09-24 23:15:38 +0530

    added mongodb connection and method to create session

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var db = require('./dbs')
+
+var bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 4200;
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static(__dirname + '/node_modules'));
+app.use('/api', require('./controllers/sessions'))
+
 
 io.on('connection', (socket) => {
     let client_id = socket.id
@@ -27,6 +35,16 @@ io.on('connection', (socket) => {
     });
 })
 
-server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+db.connect('mongodb://localhost:27017/ekipi_local', function(err) {
+  if (err) {
+    console.log('Unable to connect to Mongo.')
+    process.exit(1)
+  } else {
+    server.listen(PORT, function() {
+      console.log(`Server is listening on port ${PORT}`)
+    })
+  }
 })
+/* server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+}) */
